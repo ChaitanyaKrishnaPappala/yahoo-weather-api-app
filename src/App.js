10fc17d04266a172a9b74285f1077a1a92f397f6
Surendra Vanteddu <surendra.vanteddu@gmail.com>
2019-04-25 09:52:32 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home  from './screens/Home';
+import Login from "./components/Login";
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Yahoo Weather app</h1>
-        </header>
-        <Home/>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/' render={() => {
+              return (<div>
+                  <header className="App-header">
+                      <h1 className="App-title">Welcome to Yahoo Weather app</h1>
+                  </header>
+                  <Home/>
+              </div>)
+          }}/>
       </div>
     );
   }
